76bc77bacc8e3cfe3e07853e0fbb004e87483f28
Jefferson Gomes <30440307+fergo8@users.noreply.github.com>
2025-07-05 01:26:43 -0300

    Aula 1 do Módulo 2

---
+++ RevisaoCsharpBasico/ModuloDois/Aulas/AulaUm.CriacaoDeClasses.cs
@@ -0,0 +1,34 @@
+﻿using ModuloDois.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloDois.Aulas
+{
+    public class AulaUm
+    {
+        /***
+         * ========== AULA 1 ==============
+         * 
+         * ================================
+        ***/
+
+        public void CalcularAreaDeTriangulo()
+        {
+            Triangulo triangulo = new Triangulo();
+            Console.WriteLine("Digite os valores de cada lado do triângulo:");
+
+            triangulo.A = double.Parse(Console.ReadLine() ?? "0");
+            triangulo.B = double.Parse(Console.ReadLine() ?? "0");
+            triangulo.C = double.Parse(Console.ReadLine() ?? "0");
+
+            double perimetro = (triangulo.A + triangulo.B + triangulo.C) / 2;
+
+            double area = Math.Sqrt(perimetro * (perimetro - triangulo.A) * (perimetro - triangulo.B) * (perimetro - triangulo.C));
+
+            Console.WriteLine($"A área do triângulo é: {area:F2}");
+        }
+    }
+}
---
+++ RevisaoCsharpBasico/ModuloDois/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ModuloDois.Aulas;
+
+namespace ModuloDois
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // ========== MÓDULO DOIS ============
+            // Aulas de Lógica de Programação
+            // =================================
+
+            args = new string[] { "-aula-1" };
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Nenhuma opção foi passada como argumento.");
+                return;
+            }
+
+            foreach (var arg in args)
+            {
+                switch (arg)
+                {
+                    case "-aula-1":
+                        AulaUm aulaUm = new AulaUm();
+                        aulaUm.CalcularAreaDeTriangulo();
+                        break;
+                    default:
+                        Console.WriteLine("Nenhuma opção foi passada como argumento.");
+                        break;
+                }
+            }
+        }
+    }
+}
